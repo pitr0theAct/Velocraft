1dea35b2b50fd9529582371aab0bc3cc5fc4e8d3
Ilya Tikhonov <it.ilyatikhonov@gmail.com>
2026-05-02 18:45:43 +0200

    Добавил страницу категории "Седло"

---
+++ Demo/PageObjects/Velocraft/VelocraftSaddlePage.cs
@@ -0,0 +1,58 @@
+﻿using Demo.SeleniumFramework;
+
+namespace Demo.PageObjects.Velocraft
+{
+    public class VelocraftSaddlePage
+    {
+        WebItemWrap AddToTheAssemblyButton =>
+           new WebItemWrap("//button[contains(@class, 'button_add')]",
+               "Нажать на кнопку 'Добавить в сборку'");
+
+        // Подседельный штырь
+        WebItemWrap SeatpostImage =>
+            new WebItemWrap("//section[contains(@class, 'content-catalog__catalog-container')]//img[@alt='Fox Racing Shox Transfer SL Performance Elite 125 mm Seatpost']",
+                "Картинка подседельного штыря");
+
+        WebItemWrap SeatpostImageInDetails =>
+            new WebItemWrap("//section[contains(@class, 'content-info__item')]//img[@alt='Fox Racing Shox Transfer SL Performance Elite 125 mm Seatpost']",
+                "Картинка подседельного штыря в детальном просмотре");
+
+        // Рычаг дроппера
+        WebItemWrap DropperLevelImage =>
+            new WebItemWrap("//section[contains(@class, 'content-catalog__catalog-container')]//img[@alt='Hope Dropper Lever Handlebar Remote']",
+                "Картинка дроппера");
+
+        WebItemWrap DropperLevelImageInDetails =>
+            new WebItemWrap("//section[contains(@class, 'content-info__item')]//img[contains(@alt, 'Hope Dropper Lever Handlebar Remote')]",
+                "Картинка дроппера в детальном просмотре");
+
+        // Седло
+        WebItemWrap SaddleImage =>
+            new WebItemWrap("//section[contains(@class, 'content-catalog__catalog-container')]//img[@alt='Chromag Overture LTD Saddle']",
+                "Картинка седла");
+
+        WebItemWrap SaddleImageInDetails =>
+            new WebItemWrap("//section[contains(@class, 'content-info__item')]//img[contains(@alt, 'Chromag Overture LTD Saddle')]",
+                "Картинка седла в детальном просмотре");
+
+        public VelocraftHomePageIlya ChoosingPartsOfTheSaddle()
+        {
+            // Подседельный штырь
+            SeatpostImage.WaitDisplayed();
+            SeatpostImage.Click();
+            SeatpostImageInDetails.WaitDisplayed();
+            AddToTheAssemblyButton.Click();
+            // Рычаг дроппера
+            DropperLevelImage.WaitDisplayed();
+            DropperLevelImage.Click();
+            DropperLevelImageInDetails.WaitDisplayed();
+            AddToTheAssemblyButton.Click();
+            // Седло
+            SaddleImage.WaitDisplayed();
+            SaddleImage.Click();
+            SaddleImageInDetails.WaitDisplayed();
+            AddToTheAssemblyButton.Click();
+            return new VelocraftHomePageIlya();
+        }
+    }
+}
